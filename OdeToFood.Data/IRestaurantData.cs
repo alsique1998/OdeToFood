fee6fbced94e6712edc17db533cd92d3b2ba340e
Abner Landim Siqueira <abner.l.siqueira@exxonmobil.com>
2021-07-05 15:24:24 -0300

    feat: list restaurants page

---
+++ OdeToFood.Data/IRestaurantData.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using OdeToFood.Core;
+using System.Linq;
+
+namespace OdeToFood.Data
+{
+  public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetAll();
+    }
+
+    public class InMemoryRestaurantData: IRestaurantData
+    {
+        List<Restaurant> restaurants; 
+
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant { Id = 1, Name = "Abner's Pizza", Location = "Curitiba", Cuisine = CuisineType.Italian },
+                new Restaurant { Id = 2, Name = "Pedro's Taco", Location = "Sao Paulo", Cuisine = CuisineType.Mexican },
+                new Restaurant { Id = 3, Name = "Marcia's Cuisine", Location = "Brasilia", Cuisine = CuisineType.None }
+            };
+        }
+
+    public IEnumerable<Restaurant> GetAll() => from r in restaurants
+                                               orderby r.Name
+                                               select r;
+  }
+}
